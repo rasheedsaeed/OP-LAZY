6a2030dba85e4292bc223c14ed45ff0c7f90885f
rasheedsaeed <rasheedsaeed@protonamil.com>
2021-12-28 21:46:11 +0000

    Additional fields added to the APplication class to handle form details.

--- application_class.py
+++ application_class.py
@@ -1,9 +1,14 @@
 class Application:
-    def __init__(self, email_address: str, password: str, job_title: str, job_location: str) -> None:
+    def __init__(self, email_address: str, password: str, full_name: str, message: str, target_cv_name: str, job_title: str, job_location: str) -> None:
         self.email_address = email_address
         self.password = password
+
         self.job_title = job_title
         self.job_location = job_location
+
+        self.full_name = full_name
+        self.message = message
+        self.target_cv_name = target_cv_name
 
     def __str__(self) -> str:
         return f"{self.email_address}. Wants a {self.job_title} job in {self.job_location}"
--- main.py
+++ main.py
@@ -7,7 +7,21 @@ def unpack_sys_arg_values(values: list):
     return (values[1], values[2], values[3], values[4]) 
 
 if __name__ == "__main__":
-    email_address, password, job_title, job_location = unpack_sys_arg_values(sys.argv)
-    application = Application(email_address, password, job_title, job_location)
+    email_address = "wnvpgxegqfvqzbftxd@bvhrk.com"
+    password = "Password1!"
+    job_title = "Cleaner"
+    job_location = "Wirral"
+    full_name = "Claire Shakeshaft"
+    message = "I have 20 years of experiance with many years of supervising & managamenet."
+    target_cv_name = "Claire_Shakeshaft___CV.pdf"
 
+    application = Application(
+        email_address = email_address,
+        password = password,
+        job_title = job_title,
+        job_location = job_location,
+        full_name = full_name,
+        message = message,
+        target_cv_name = target_cv_name
+    )
     apply_for_jobs(application)
