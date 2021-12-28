434ebc00be37a032ae57081e983dc75229490817
rasheedsaeed <rasheedsaeed@protonamil.com>
2021-12-28 19:16:01 +0000

    We can now sucessfully login.

--- apply_for_job_script.py
+++ apply_for_job_script.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 def apply_for_job(application: "Application"):
     driver = setup_driver()
@@ -14,6 +15,20 @@ def setup_driver() -> webdriver:
 def login(driver: webdriver, application: "Application"):
     driver.get("https://findajob.dwp.gov.uk/sign-in")
 
+    # Enter credentials
     email_input_form_id = "email"
+    email_input_form_element = driver.find_element_by_id(email_input_form_id)
+    email_input_form_element.clear()
+    email_input_form_element.send_keys(application.email_address)
+
     password_input_form_id = "password"
+    password_input_form_element = driver.find_element_by_id(password_input_form_id)
+    password_input_form_element.clear()
+
+    # Once we've entered our password, we'll hit ENTER to login... this saves us finding and clicking the submit button
+    password_input_form_element.send_keys(application.password, Keys.ENTER)
+
+
+
+
 
