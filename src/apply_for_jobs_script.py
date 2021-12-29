76c6c5c94a5f74aad96460c1840055744e47fd43
rasheedsaeed <rasheedsaeed@protonamil.com>
2021-12-29 10:31:06 +0000

    Removed unused exception

--- src/apply_for_jobs_script.py
+++ src/apply_for_jobs_script.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import TimeoutException
 
 import time
 
